544e4a1d7a95a7a7cc2355d003071b09181f61e5
rfrohl <frohl.robert@gmail.com>
2014-06-05 16:21:06 -0700

    add - install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/gdbinit ~/.gdbinit
+ln -s ~/.dotfiles/hgrc ~/.hgrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
