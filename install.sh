24df1f93b33d32b737384a7f940bbda47de87365
rfrohl <frohl.robert@gmail.com>
2014-06-05 22:49:22 -0700

    add - gitignore

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/gdbinit ~/.gdbinit
 ln -s ~/.dotfiles/hgrc ~/.hgrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore ~/.gitignore
