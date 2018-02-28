d11e617f9f9059d9de9ae0e61007e953c3387e41
rfrohl <frohl.robert@gmail.com>
2018-02-28 20:47:26 +0100

    add gpg.conf

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/gdbinit ~/.gdbinit
+ln -s ~/.dotfiles/ansible.cfg ~/.ansible.cfg
 ln -s ~/.dotfiles/hgrc ~/.hgrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/ansible.cfg ~/.ansible.cfg
+ln -s ~/.dotfiles/gpg.conf ~/.gnupg/gpg.conf
