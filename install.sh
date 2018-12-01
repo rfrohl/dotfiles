87eb4adac94de039e4552c4e2ceba7dcf111a40c
rfrohl <frohl.robert@gmail.com>
2018-12-01 14:35:56 +0100

    add nftables workstation rules

--- install.sh
+++ install.sh
@@ -16,6 +16,14 @@ ln -s ~/.dotfiles/gpg.conf ~/.gnupg/gpg.conf
 # security related
 [[ -e ~/.ssh && ! -e ~/.ssh/config ]] && cp ~/.dotfiles/ssh_config ~/.ssh/config
 
+cat << EOF
+for nftables firewall (as root):
+  cp ~/.dotfiles/firewall/workstation.nftables /etc/nftables.conf
+  vim /etc/nftables.conf
+  systemctl start nftables
+  systemctl enable nftables
+EOF
+
 # advanced tooling
 [[ ! -e ~/.newsboat ]] && mkdir ~/.newsboat
 ln -s ~/.dotfiles/newsboat.cfg ~/.newsboat/config
